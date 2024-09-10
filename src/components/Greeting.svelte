d7fa4f4df69f4b66e1307a39818737f4bacc0ba9
Daniel Payan <dany.sasket@gmail.com>
2024-09-10 14:57:50 -0600

    :lipstick: feat-add-pages

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    const currentTime = new Date().toLocaleTimeString();
+    const currentHour = new Date().getHours();
+
+    let greeting = "";
+
+    if(currentHour < 12) {
+        greeting = "Good Morning";
+    } else if(currentHour < 18) {
+        greeting = "Good Afternoon";
+    } else {
+        greeting = "Good Evening";
+    }
+</script>
+
+<h1 class="text-3xl font-bold">
+    {greeting}
+</h1>
